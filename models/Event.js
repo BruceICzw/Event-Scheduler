dde4ac73b2a9f0553e591da67d6ebe4df4d5b38c
BruceICzw <h220396z@hit.ac.zw>
2024-03-07 00:42:11 +0200

    Create Models

---
+++ models/Event.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+
+    },
+    description: {
+        type: String,
+        required: true,
+
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('Event', eventSchema);
