82ea8584e71a6745aafa0586325374df92ef5e45
BruceICzw <h220396z@hit.ac.zw>
2024-03-07 00:37:16 +0200

    Connect to Database

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
-
+const mongoose = require('mongoose');
 const app = express();
 
-
+//Conect to Database
+mongoose.connect('mongodb://127.0.0.1:27017/EventSchedulerDB', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB...');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB...', err);
+    });
 
 const PORT = process.env.PORT || 3000;
 
