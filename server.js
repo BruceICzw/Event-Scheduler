024698751360aa4cda6663d81c1b123911f4918c
BruceICzw <h220396z@hit.ac.zw>
2024-03-07 10:40:37 +0200

    Fix database connection

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose');
+const connectDB = require('./database/db');
 const app = express();
 
 //Conect to Database
-mongoose.connect('mongodb://127.0.0.1:27017/EventSchedulerDB', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB...');
-    })
-    .catch((err) => {
-        console.log('Error connecting to MongoDB...', err);
-    });
-
+connectDB()
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
